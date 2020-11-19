0cc6563eda768349f1925f2fe759d77a7944c769
Josh <josh.londt@gmail.com>
2020-11-19 13:36:47 +0800

    Linking to database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,16 +3,24 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database";
 
+// const config = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+// };
+
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: 'AIzaSyAsbE99suyNgPSOdvQmfoOaNoUb_FoTlkM',
+  authDomain: 'curriculum-aide.firebaseapp.com',
+  databaseURL: 'https://curriculum-aide.firebaseio.com',
+  projectId: 'curriculum-aide',
+  storageBucket: 'curriculum-aide.appspot.com',
+  messagingSenderId: '51333740661'
 };
 
-console.log(config);
 
 firebase.initializeApp(config);
 
