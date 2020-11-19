e3223bd448772f2659b7cc73429145856ebfd781
Josh <josh.londt@gmail.com>
2020-11-19 14:19:56 +0800

    Added config to heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,22 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database";
 
-// const config = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASE_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-// };
-
 const config = {
-  apiKey: 'AIzaSyAsbE99suyNgPSOdvQmfoOaNoUb_FoTlkM',
-  authDomain: 'curriculum-aide.firebaseapp.com',
-  databaseURL: 'https://curriculum-aide.firebaseio.com',
-  projectId: 'curriculum-aide',
-  storageBucket: 'curriculum-aide.appspot.com',
-  messagingSenderId: '51333740661'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 
